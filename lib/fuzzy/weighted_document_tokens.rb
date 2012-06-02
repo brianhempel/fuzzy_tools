5f2a4124bc55807bb6ce19b4272af7372eb8f27a
Brian Hempel <bnhempel@mtu.edu>
2012-06-02 04:21:39 -0400

    Write cosine_similarity in C w/RubyInline
    430s -> 237s
    
          Min: 37.878788 (bible_books_asv_abbreviations.csv)
         Mean: 85.796106
       Median: 95.857988
          Max: 100.000000
    
        SCORE: 73.177627
    
         Time: 236.6s
    
    Testfiles: 23

--- lib/fuzzy/weighted_document_tokens.rb
+++ lib/fuzzy/weighted_document_tokens.rb
@@ -1,4 +1,5 @@
 require 'fuzzy/helpers'
+require 'inline'
 
 module Fuzzy
   class WeightedDocumentTokens
@@ -10,14 +11,43 @@ module Fuzzy
     end
 
     def cosine_similarity(other)
-      similarity = 0.0
-      other_weights = other.weights
-      weights.each do |token, weight|
-        if other_weight = other_weights[token]
-          similarity += other_weight*weight
-        end
-      end
-      similarity
+      cosine_similarity_fast(other)
+      # equivalent to the RUBY below, and 2x faster
+      # similarity = 0.0
+      # other_weights = other.weights
+      # @weights.each do |token, weight|
+      #   if other_weight = other_weights[token]
+      #     similarity += other_weight*weight
+      #   end
+      # end
+      # similarity
+    end
+
+    inline(:C) do |builder|
+      builder.c_raw <<-EOC
+        static VALUE cosine_similarity_fast(int argc, VALUE *argv, VALUE self) {
+          double similarity    = 0.0;
+          VALUE  other         = argv[0];
+          VALUE  other_weights = rb_funcall(other, rb_intern("weights"), 0);
+          VALUE  my_weights    = rb_ivar_get(self, rb_intern("@weights"));
+          VALUE  my_tokens     = rb_funcall(self, rb_intern("tokens"), 0);
+          int    i;
+          VALUE  token;
+          VALUE  my_weight;
+          VALUE  other_weight;
+
+          for(i = 0; i < RARRAY(my_tokens)->len; i++) {
+            token        = RARRAY(my_tokens)->ptr[i];
+            my_weight    = rb_hash_aref(my_weights, token);
+            other_weight = rb_hash_aref(other_weights, token);
+            if (other_weight != Qnil) {
+              similarity += NUM2DBL(my_weight)*NUM2DBL(other_weight);
+            }
+          }
+
+          return rb_float_new(similarity);
+        }
+      EOC
     end
 
     def tokens

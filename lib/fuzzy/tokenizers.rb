7a6b684cfc39adaabff3d426c31f2121a92e9f77
Brian Hempel <bnhempel@mtu.edu>
2012-06-05 00:44:50 -0400

    Use downcasing tokenizer. Better results.
    SCORE: 73.2 -> 76.9 (Mean: 85.8% -> 90.8%)
    Time: 126s -> 134s
    
           Min: 44.067797 (second_string_va_univ.csv)
          Mean: 90.778755
        Median: 95.857988
           Max: 100.000000
    
         SCORE: 76.901513
    
          Time: 134.3s
    
     Testfiles: 23

--- lib/fuzzy/tokenizers.rb
+++ lib/fuzzy/tokenizers.rb
@@ -1,12 +1,19 @@
 module Fuzzy
   module Tokenizers
-    CHARACTERS = lambda { |str| str.chars }
-    BIGRAMS    = lambda { |str| Fuzzy::Helpers.ngrams(str, 2) }
-    TRIGRAMS   = lambda { |str| Fuzzy::Helpers.ngrams(str, 3) }
-    TETRAGRAMS = lambda { |str| Fuzzy::Helpers.ngrams(str, 4) }
-    PENTAGRAMS = lambda { |str| Fuzzy::Helpers.ngrams(str, 5) }
-    HEXAGRAMS  = lambda { |str| Fuzzy::Helpers.ngrams(str, 6) }
+    CHARACTERS           = lambda { |str| str.chars }
+    CHARACTERS_DOWNCASED = lambda { |str| str.chars.map(&:downcase) }
+    BIGRAMS              = lambda { |str| Fuzzy::Helpers.ngrams(str, 2) }
+    BIGRAMS_DOWNCASED    = lambda { |str| Fuzzy::Helpers.ngrams(str, 2).map(&:downcase) }
+    TRIGRAMS             = lambda { |str| Fuzzy::Helpers.ngrams(str, 3) }
+    TRIGRAMS_DOWNCASED   = lambda { |str| Fuzzy::Helpers.ngrams(str, 3).map(&:downcase) }
+    TETRAGRAMS           = lambda { |str| Fuzzy::Helpers.ngrams(str, 4) }
+    TETRAGRAMS_DOWNCASED = lambda { |str| Fuzzy::Helpers.ngrams(str, 4).map(&:downcase) }
+    PENTAGRAMS           = lambda { |str| Fuzzy::Helpers.ngrams(str, 5) }
+    PENTAGRAMS_DOWNCASED = lambda { |str| Fuzzy::Helpers.ngrams(str, 5).map(&:downcase) }
+    HEXAGRAMS            = lambda { |str| Fuzzy::Helpers.ngrams(str, 6) }
+    HEXAGRAMS_DOWNCASED  = lambda { |str| Fuzzy::Helpers.ngrams(str, 6).map(&:downcase) }
 
-    WORDS      = lambda { |str| str.split }
+    WORDS                = lambda { |str| str.split }
+    WORDS_DOWNCASED      = lambda { |str| str.split.map(&:downcase) }
   end
 end

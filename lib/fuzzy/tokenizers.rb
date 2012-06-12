565d5fa8c71016106fd14d5d4e530dfe549c11b7
Brian Hempel <bnhempel@mtu.edu>
2012-06-12 01:44:56 -0400

    Downcase first for more efficient tokenizers
    82s -> 79s
    
           Min: 44.067797 (second_string_va_univ.csv)
          Mean: 90.778755
        Median: 95.857988
           Max: 100.000000
    
         SCORE: 76.901513
    
          Time: 78.9s
    
     Testfiles: 23

--- lib/fuzzy/tokenizers.rb
+++ lib/fuzzy/tokenizers.rb
@@ -1,19 +1,19 @@
 module Fuzzy
   module Tokenizers
     CHARACTERS           = lambda { |str| str.chars }
-    CHARACTERS_DOWNCASED = lambda { |str| str.chars.map(&:downcase) }
-    BIGRAMS              = lambda { |str| Fuzzy::Helpers.ngrams(str, 2) }
-    BIGRAMS_DOWNCASED    = lambda { |str| Fuzzy::Helpers.ngrams(str, 2).map(&:downcase) }
-    TRIGRAMS             = lambda { |str| Fuzzy::Helpers.ngrams(str, 3) }
-    TRIGRAMS_DOWNCASED   = lambda { |str| Fuzzy::Helpers.ngrams(str, 3).map(&:downcase) }
-    TETRAGRAMS           = lambda { |str| Fuzzy::Helpers.ngrams(str, 4) }
-    TETRAGRAMS_DOWNCASED = lambda { |str| Fuzzy::Helpers.ngrams(str, 4).map(&:downcase) }
-    PENTAGRAMS           = lambda { |str| Fuzzy::Helpers.ngrams(str, 5) }
-    PENTAGRAMS_DOWNCASED = lambda { |str| Fuzzy::Helpers.ngrams(str, 5).map(&:downcase) }
-    HEXAGRAMS            = lambda { |str| Fuzzy::Helpers.ngrams(str, 6) }
-    HEXAGRAMS_DOWNCASED  = lambda { |str| Fuzzy::Helpers.ngrams(str, 6).map(&:downcase) }
+    CHARACTERS_DOWNCASED = lambda { |str| str.downcase.chars }
+    BIGRAMS              = lambda { |str| Fuzzy::Helpers.ngrams(str,          2) }
+    BIGRAMS_DOWNCASED    = lambda { |str| Fuzzy::Helpers.ngrams(str.downcase, 2) }
+    TRIGRAMS             = lambda { |str| Fuzzy::Helpers.ngrams(str,          3) }
+    TRIGRAMS_DOWNCASED   = lambda { |str| Fuzzy::Helpers.ngrams(str.downcase, 3) }
+    TETRAGRAMS           = lambda { |str| Fuzzy::Helpers.ngrams(str,          4) }
+    TETRAGRAMS_DOWNCASED = lambda { |str| Fuzzy::Helpers.ngrams(str.downcase, 4) }
+    PENTAGRAMS           = lambda { |str| Fuzzy::Helpers.ngrams(str,          5) }
+    PENTAGRAMS_DOWNCASED = lambda { |str| Fuzzy::Helpers.ngrams(str.downcase, 5) }
+    HEXAGRAMS            = lambda { |str| Fuzzy::Helpers.ngrams(str,          6) }
+    HEXAGRAMS_DOWNCASED  = lambda { |str| Fuzzy::Helpers.ngrams(str.downcase, 6) }
 
     WORDS                = lambda { |str| str.split }
-    WORDS_DOWNCASED      = lambda { |str| str.split.map(&:downcase) }
+    WORDS_DOWNCASED      = lambda { |str| str.downcase.split }
   end
 end

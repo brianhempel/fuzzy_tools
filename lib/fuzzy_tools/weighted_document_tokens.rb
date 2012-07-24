fd3d17a4b7775837e30e475dbc5a24b09fd2ae0b
Brian Hempel <bnhempel@mtu.edu>
2012-07-23 22:25:19 -0400

    Ostensibly working cosine_similarity for non-RubyInline interpreters
    Rubinius still gets all the test data wrong, however

--- lib/fuzzy_tools/weighted_document_tokens.rb
+++ lib/fuzzy_tools/weighted_document_tokens.rb
@@ -1,5 +1,4 @@
 require 'fuzzy_tools/helpers'
-require 'inline'
 
 module FuzzyTools
   class WeightedDocumentTokens
@@ -10,43 +9,57 @@ module FuzzyTools
       set_token_weights(tokens, &weight_function)
     end
 
-    def cosine_similarity(other)
-      cosine_similarity_fast(@weights, tokens, other.weights)
-      # equivalent to the C below, but the C is >2x faster
-      # similarity = 0.0
-      # other_weights = other.weights
-      # @weights.each do |token, weight|
-      #   if other_weight = other_weights[token]
-      #     similarity += other_weight*weight
-      #   end
-      # end
-      # similarity
-    end
+    if RUBY_DESCRIPTION !~ /^ruby/
 
-    inline(:C) do |builder|
-      builder.c_raw <<-EOC
-        static VALUE cosine_similarity_fast(int argc, VALUE *argv, VALUE self) {
-          double similarity    = 0.0;
-          VALUE  my_weights    = argv[0];
-          VALUE  my_tokens     = argv[1];
-          VALUE  other_weights = argv[2];
-          int    i;
-          VALUE  token;
-          VALUE  my_weight;
-          VALUE  other_weight;
+      # Rubinius and JRuby
+      def cosine_similarity(other)
+        # equivalent to the C below, but the C is >2x faster
+        similarity = 0.0
+        other_weights = other.weights
+        @weights.each do |token, weight|
+          if other_weight = other_weights[token]
+            similarity += other_weight*weight
+          end
+        end
+        similarity
+      end
 
-          for(i = 0; i < RARRAY_LEN(RARRAY(my_tokens)); i++) {
-            token        = RARRAY_PTR(RARRAY(my_tokens))[i];
-            other_weight = rb_hash_aref(other_weights, token);
-            if (other_weight != Qnil) {
-              my_weight   = rb_hash_aref(my_weights, token);
-              similarity += NUM2DBL(my_weight)*NUM2DBL(other_weight);
+    else
+
+      # MRI
+
+      require 'inline'
+
+      def cosine_similarity(other)
+        cosine_similarity_fast(@weights, tokens, other.weights)
+      end
+
+      inline(:C) do |builder|
+        builder.c_raw <<-EOC
+          static VALUE cosine_similarity_fast(int argc, VALUE *argv, VALUE self) {
+            double similarity    = 0.0;
+            VALUE  my_weights    = argv[0];
+            VALUE  my_tokens     = argv[1];
+            VALUE  other_weights = argv[2];
+            int    i;
+            VALUE  token;
+            VALUE  my_weight;
+            VALUE  other_weight;
+
+            for(i = 0; i < RARRAY_LEN(RARRAY(my_tokens)); i++) {
+              token        = RARRAY_PTR(RARRAY(my_tokens))[i];
+              other_weight = rb_hash_aref(other_weights, token);
+              if (other_weight != Qnil) {
+                my_weight   = rb_hash_aref(my_weights, token);
+                similarity += NUM2DBL(my_weight)*NUM2DBL(other_weight);
+              }
             }
-          }
 
-          return rb_float_new(similarity);
-        }
-      EOC
+            return rb_float_new(similarity);
+          }
+        EOC
+      end
+
     end
 
     def tokens
